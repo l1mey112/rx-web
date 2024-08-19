c81ca9bfc94ace4859b494bafb3701b361098d70
l1mey112 <l-m@l-m.dev>
2024-08-19 22:58:44 +1000

    init

---
+++ argon2/main.c
@@ -0,0 +1,27 @@
+/* #include <stdint.h>
+#include <stdlib.h>
+#define ALIGNMENT 8
+#define PAGE_SIZE 65536
+#define NULL (void*)0
+
+extern void __heap_base;
+static void *bump = NULL;
+
+void *m(size_t size) {
+	if (bump == NULL)
+		bump = &__heap_base;
+	void *ret = (void*)(((uintptr_t)bump + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1));
+	if (((uintptr_t)ret + size) > __builtin_wasm_memory_size(0) * PAGE_SIZE)
+		__builtin_wasm_memory_grow(0, size / PAGE_SIZE);
+	bump = (char*)ret + size;
+	return ret;
+} */
+
+int test(int *g) {
+	return *g;
+}
+
+int tester() {
+	int g = 50;
+	return test(&g);
+}
